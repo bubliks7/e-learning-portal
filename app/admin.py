ce4777ccb5328f0071caeefcde9a9b2beb71aae9
Jakub <bubliksiur@gmail.com>
2025-10-23 10:48:40 +0200

    fix admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Kurs, Test, Pytanie, Odpowiedz
+from .models import Kursy, Testy, Pytania, Odpowiedzi
 
-admin.site.register(Kurs)
-admin.site.register(Test)
-admin.site.register(Pytanie)
-admin.site.register(Odpowiedz)
+admin.site.register(Kursy)
+admin.site.register(Testy)
+admin.site.register(Pytania)
+admin.site.register(Odpowiedzi)
