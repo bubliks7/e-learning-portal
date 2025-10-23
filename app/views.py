2301c80fdd06d214e0128d60415ae2743ea11404
Jakub <bubliksiur@gmail.com>
2025-10-23 10:50:11 +0200

    fix admin panel

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Kurs
+from .models import Kursy
 
 def course_list(request):
-    courses = Kurs.objects.all()
+    courses = Kursy.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
 
 def course_detail(request, pk):
-    course = get_object_or_404(Kurs, pk=pk)
+    course = get_object_or_404(Kursy, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
