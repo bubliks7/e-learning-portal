6137ce2f153e7420384fa9bd9fd4f235a5879140
Jakub <bubliksiur@gmail.com>
2025-10-23 14:43:48 +0200

    add home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 from portal import urls
 
 urlpatterns = [
-    path('', include('portal.urls')),
+    path('', include('portal.urls')), 
+    path('courses/', include('portal.urls')),
     path('admin/', admin.site.urls),
 ]
--- portal/urls.py
+++ portal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),
+    path('', views.home, name='home'),
+    path('courses/', views.course_list, name='course_list'),
     path('courses/<int:pk>/', views.course_detail, name='course_detail'),
 ]
