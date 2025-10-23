a33ea4cfd683af7c4fe3d8c2043fe1ba79b5b548
Jakub <bubliksiur@gmail.com>
2025-10-23 22:52:28 +0200

    fix login

--- portal/urls.py
+++ portal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login_user', views.login_user, name="login"),
     path('courses/', views.course_list, name='course_list'),
     path('courses/<int:pk>/', views.course_detail, name='course_detail'),
+    path('login/', views.login_user, name='login'),
 ]
