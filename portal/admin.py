a0ceb0ec4e654e340047009a25391f28a669b955
Jakub <bubliksiur@gmail.com>
2025-10-23 11:35:43 +0200

    updated admin panel

--- portal/admin.py
+++ portal/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Kursy, Testy, Pytania, Odpowiedzi
+from .models import Kursy, Testy, Pytania, Odpowiedzi, Uzytkownicy
 
 admin.site.register(Kursy)
 admin.site.register(Testy)
 admin.site.register(Pytania)
 admin.site.register(Odpowiedzi)
+admin.site.register(Uzytkownicy)
