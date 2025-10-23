d9ef7fc92e94ea2d275c71493ef8c890307c5749
Jakub <bubliksiur@gmail.com>
2025-10-23 23:00:56 +0200

    fix login error

--- portal/urls.py
+++ portal/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('courses/', views.course_list, name='course_list'),
     path('courses/<int:pk>/', views.course_detail, name='course_detail'),
-    path('login/', views.login_user, name='login'),
 ]
