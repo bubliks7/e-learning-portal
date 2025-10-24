8d71d64d0a1ab20f2a559c626f631e39831be6b3
Jakub <bubliksiur@gmail.com>
2025-10-24 16:51:25 +0200

    perrmisions logic

--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Kursy, Enrollment
+from .models import Kursy
 
 def home(request):
     new_courses = Kursy.objects.order_by('-data_utworzenia')[:3]
