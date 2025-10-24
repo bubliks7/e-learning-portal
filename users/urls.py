38d9463109c8dbfd558fc0fbb61a9c61c7d461cb
Jakub <bubliksiur@gmail.com>
2025-10-24 10:18:17 +0200

    fix urls

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('/users/register', views.register_view, name='register_view'),
 ]
