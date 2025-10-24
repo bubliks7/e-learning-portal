5cf9bac03e9fc9f26810623ba5cc454ea66b522f
Jakub <bubliksiur@gmail.com>
2025-10-24 12:13:27 +0200

    added logout logic

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register_view, name='register_view'),
     path('login/', views.login_view, name='login_view'),
+    path('logout/', views.logout_view, name='logout_view'),
 ]
