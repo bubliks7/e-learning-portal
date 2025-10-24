c9893ac3ac6cdab512360828a942d65dc456bfd2
Jakub <bubliksiur@gmail.com>
2025-10-24 12:39:14 +0200

    fix errors

--- portal/urls.py
+++ portal/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = "portal"
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('courses/', views.course_list, name='course_list'),
     path('courses/<int:pk>/', views.course_detail, name='course_detail'),
-    # path('new_portal/', views.new_portal, name='new_portal'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Kursy
 from django.contrib.auth.decorators import login_required
+from .models import Kursy
 
 def home(request):
     new_courses = Kursy.objects.order_by('-data_utworzenia')[:3]
@@ -13,7 +13,3 @@ def course_list(request):
 def course_detail(request, pk):
     course = get_object_or_404(Kursy, pk=pk)
     return render(request, 'portal/course_detail.html', {'course': course})
-
-# @login_required(login_url="/users/login/")
-# def new_portal(request):
-#     return render(request, 'portal/new_portal.html')
