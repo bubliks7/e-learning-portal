ce6a2f56f7d82cb19215f8e157199cdd12b3d140
Jakub <bubliksiur@gmail.com>
2025-10-28 17:21:06 +0100

    fix

--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Kursy
-from .models import Uzytkownicy
+from portal.models import Uzytkownicy
 
 def home(request):
     new_courses = Kursy.objects.order_by('-data_utworzenia')[:3]
@@ -17,5 +17,5 @@ def course_detail(request, pk):
 
 @login_required
 def profile(request):
-    profil = request.user.uzytkownicy
+    profil = Uzytkownicy.objects.get(user=request.user)
     return render(request, 'portal/profile.html', {'profil': profil})
