fe2f18faf4255956e0d64d8e0c88015d3fa4f2d9
Jakub <bubliksiur@gmail.com>
2025-10-30 17:00:51 +0100

    added new register

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    # path('register/', views.register_view, name='register_view'),
     path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
-    # path('register/', loginForm.as_view(), name='register'),
 ]
