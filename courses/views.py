80031445910623ff309f6b9f8ac078e9b1034df0
Jakub <bubliksiur@gmail.com>
2025-10-31 19:17:11 +0100

    fix

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,6 @@ from portal.models import Kursy, Uzytkownicy
 @login_required
 def enroll_course(request, kurs_id):
     kurs = get_object_or_404(Kursy, id=kurs_id)
-    uzytkownik = get_object_or_404(Uzytkownicy, user=request.user)
+    uzytkownik = get_object_or_404(Uzytkownicy, email=request.user.email)
     Enrollment.objects.get_or_create(uzytkownik=uzytkownik, kurs=kurs)
     return redirect('course_detail', kurs_id=kurs.id)
