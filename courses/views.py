8d48cc928ac6abac618b554b3f9d3d28dbbd7fae
Jakub <bubliksiur@gmail.com>
2025-10-31 19:08:26 +0100

    fix

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Enrollment
-from portal.models import Kursy
+from portal.models import Kursy, Uzytkownicy
 
 @login_required
 def enroll_course(request, kurs_id):
     kurs = get_object_or_404(Kursy, id=kurs_id)
-    Enrollment.objects.get_or_create(uzytkownik=request.user, kurs=kurs)
+    uzytkownik = get_object_or_404(Uzytkownicy, user=request.user)
+    Enrollment.objects.get_or_create(uzytkownik=uzytkownik, kurs=kurs)
     return redirect('course_detail', kurs_id=kurs.id)
