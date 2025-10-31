108f97d7039bc258fdd72d5edecc0eda1f6d5420
Jakub <bubliksiur@gmail.com>
2025-10-31 18:01:10 +0100

    added new app

--- portal/urls.py
+++ portal/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('courses/', views.course_list, name='course_list'),
-    path('courses/<int:pk>/', views.course_detail, name='course_detail'),
+    # path('courses/<int:pk>/', views.course_detail, name='course_detail'),
     path('accound/', views.accound, name='accound'),
     path('register/', views.register, name='register'),
 ]
