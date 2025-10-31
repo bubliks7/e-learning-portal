28ac25e23bf01b6923f20f0edc18d45fc100f61b
Jakub <bubliksiur@gmail.com>
2025-10-31 19:20:21 +0100

    fix

--- courses/views.py
+++ courses/views.py
@@ -8,4 +8,5 @@ def enroll_course(request, kurs_id):
     kurs = get_object_or_404(Kursy, id=kurs_id)
     uzytkownik = get_object_or_404(Uzytkownicy, email=request.user.email)
     Enrollment.objects.get_or_create(uzytkownik=uzytkownik, kurs=kurs)
-    return redirect('course_detail', kurs_id=kurs.id)
+    return redirect('course_detail', pk=kurs.id)
+
