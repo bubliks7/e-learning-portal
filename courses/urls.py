d023d026450646c8911dd03b9d64787d0e9c93b6
Jakub <bubliksiur@gmail.com>
2025-10-31 18:46:24 +0100

    added new app

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,6 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-
-# ]
+urlpatterns = [
+    path('enroll/<int:kurs_id>/', views.enroll_course, name='enroll_course'),
+]
