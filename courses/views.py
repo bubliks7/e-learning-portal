51f4e5976283c48f23cf449cf282bf57b585f289
Jakub <bubliksiur@gmail.com>
2025-10-31 18:43:44 +0100

    added new app

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Enrollment
+from portal.models import Kursy
 
-# Create your views here.
+@login_required
+def enroll_course(request, kurs_id):
+    kurs = get_object_or_404(Kursy, id=kurs_id)
+    Enrollment.objects.get_or_create(uzytkownik=request.uzytkownik, kurs=kurs)
+    return redirect('portal/course_detail', kurs_id=Kursy.id)
