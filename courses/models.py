b189b6f349a4aa937f4335618401317b9d9db540
Jakub <bubliksiur@gmail.com>
2025-10-31 18:32:06 +0100

    added new app

--- courses/models.py
+++ courses/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 from portal.models import Uzytkownicy
+from portal.models import Kursy
 # Create your models here.
 
-class Kursy(models.Model):
-    id = models.AutoField(primary_key=True)
-    tytul = models.CharField(max_length=200)
-    opis = models.TextField()
-    autor = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE, related_name='kursy')
-    data_utworzenia = models.DateTimeField(auto_now_add=True)
-    def __str__(self):
-        return self.tytul
-    
 class Enrollment(models.Model):
     uzytkownik = models.ForeignKey(Uzytkownicy, on_delete=models.CASCADE, related_name="enrollment")
     kurs = models.ForeignKey(Kursy, on_delete=models.CASCADE, related_name="enrollment")
