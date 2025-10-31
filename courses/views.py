591e3c66786aac84119a5bf97ec601b06ea45cde
Jakub <bubliksiur@gmail.com>
2025-10-31 19:32:47 +0100

    added enrollments

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Enrollment
 from portal.models import Kursy, Uzytkownicy
@@ -8,5 +8,6 @@ def enroll_course(request, kurs_id):
     kurs = get_object_or_404(Kursy, id=kurs_id)
     uzytkownik = get_object_or_404(Uzytkownicy, email=request.user.email)
     Enrollment.objects.get_or_create(uzytkownik=uzytkownik, kurs=kurs)
-    return redirect('course_detail', pk=kurs.id)
+    # return redirect('course_detail', pk=kurs.id)
+    return render(request, 'portal/zapisano.html', {'kurs': kurs})
 
