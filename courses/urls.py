464967440382bf64bf08f0ef647de3974a5d5663
Jakub <bubliksiur@gmail.com>
2025-10-31 23:39:53 +0100

    added myCourses

--- courses/urls.py
+++ courses/urls.py
@@ -5,5 +5,5 @@ app_name = 'saved_courses'
 
 urlpatterns = [
     path('enroll/enroll/<int:kurs_id>/', views.enroll_course, name='enroll_course'),
-    path('enrolls/', views.enroll_view, name='enroll_view'),
+    path('enrolls/', views.my_courses, name='my_courses'),
 ]
