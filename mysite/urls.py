02158d52a17be2469ec051b0161809dc5504dc64
Jakub <bubliksiur@gmail.com>
2025-11-03 22:00:50 +0100

    added styles

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
-from portal import urls
-from users import urls
 
 urlpatterns = [
     path('', include('portal.urls')),
