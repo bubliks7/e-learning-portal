3067ec0b9e303ab9a66fdbde985f43caadd91cfb
Jakub <bubliksiur@gmail.com>
2025-11-11 21:07:24 +0100

    fix template

--- courses/static/courses/script.js
+++ courses/static/courses/script.js
@@ -17,17 +17,20 @@ const pages = document.querySelectorAll(".page");
 
 // showPage(currentPage);
 
-document.querySelectorAll('.course-container').forEach(container => {
-    const left = container.querySelector('#go_left');
-    const right = container.querySelector('#go_right');
-    let current = 0;
+let current = 0;
 
-    function showPage(i) {
-        pages.forEach((p, n) => p.style.display = (n === i) ? 'block' : 'none');
-    }
+function showPage(n) {
+    pages.forEach((p, i) => p.style.display = (i === n) ? 'block' : 'none');
+}
 
-    left.addEventListener('click', () => { if (current > 0) current--; showPage(current); });
-    right.addEventListener('click', () => { if (current < pages.length - 1) current++; showPage(current); });
-
+document.getElementById('go_left').onclick = () => {
+    if (current > 0) current--;
     showPage(current);
-});
+};
+
+document.getElementById('go_right').onclick = () => {
+    if (current < pages.length - 1) current++;
+    showPage(current);
+};
+
+showPage(current);
