62450d9f4ee4dc18216ecd40be97d79931ea2890
Jakub <bubliksiur@gmail.com>
2025-11-11 21:15:07 +0100

    fix template

--- courses/static/courses/script.js
+++ courses/static/courses/script.js
@@ -1,40 +1,18 @@
-// let currentPage = 0;
+const pages = document.querySelectorAll(".page");
+let currentPage = 0;
 
-// function showPage(index) {
-//     pages.forEach((p, i) => {
-//         p.style.display = i === index ? "block" : "none";
-//     });
-// }
-// document.getElementById("go_left").addEventListener("click", () => {
-//     if (currentPage > 0) currentPage--;
-//     showPage(currentPage);
-// });
-// document.getElementById("go_right").addEventListener("click", () => {
-//     if (currentPage < pages.length - 1) currentPage++;
-//     showPage(currentPage);
-// });
-
-// showPage(currentPage);
-
-document.querySelectorAll('.course-container').forEach(container => {
-    const pages = container.querySelectorAll('.page');
-    const leftBtn = container.querySelector('.go_left');
-    const rightBtn = container.querySelector('.go_right');
-    let current = 0;
-
-    function showPage(n) {
-        pages.forEach((p, i) => p.style.display = (i === n ? 'block' : 'none'));
-    }
-
-    leftBtn.addEventListener('click', () => {
-        if (current > 0) current--;
-        showPage(current);
+function showPage(index) {
+    pages.forEach((p, i) => {
+        p.style.display = i === index ? "block" : "none";
     });
-
-    rightBtn.addEventListener('click', () => {
-        if (current < pages.length - 1) current++;
-        showPage(current);
-    });
-
-    showPage(current);
+}
+document.getElementById("go_left").addEventListener("click", () => {
+    if (currentPage > 0) currentPage--;
+    showPage(currentPage);
 });
+document.getElementById("go_right").addEventListener("click", () => {
+    if (currentPage < pages.length - 1) currentPage++;
+    showPage(currentPage);
+});
+
+showPage(currentPage);
