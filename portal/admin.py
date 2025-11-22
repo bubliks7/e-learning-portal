1dc895e96d0f13874a5f3927df67c9aa0a315ba7
Jakub <bubliksiur@gmail.com>
2025-11-22 19:12:23 +0100

    views has been fixed

--- portal/admin.py
+++ portal/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Kursy, Testy, Pytania, Odpowiedzi, Uzytkownicy
+from .models import Kursy, Uzytkownicy
 
 admin.site.register(Kursy)
-admin.site.register(Testy)
-admin.site.register(Pytania)
-admin.site.register(Odpowiedzi)
 admin.site.register(Uzytkownicy)
--- tests/admin.py
+++ tests/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from portal.models import Testy
+from portal.models import Testy, Pytania, Odpowiedzi
 # Register your models here.
 
 admin.site.register(Testy)
+admin.site.register(Pytania)
+admin.site.register(Odpowiedzi)
