62ac81db0b68be324d0ce9a8b951e8df86673d3c
root <root@LAPTOP-0PBHL1A3.localdomain>
2025-12-09 09:55:05 +0100

    update view and template

--- tests/views.py
+++ tests/views.py
@@ -5,7 +5,9 @@ from portal.models import Testy, Pytania, Odpowiedzi
 def test_view(request, pk):
     test = get_object_or_404(Testy, pk=pk)
     pytanie = get_object_or_404(Pytania, pk=pk)
+    odpowiedz = get_object_or_404(Odpowiedzi, pk=pk)
     return render(request, 'tests/test_view.html', {
         'test': test,
         'pytanie': pytanie,
+        'odpowiedz': odpowiedz,
         })
