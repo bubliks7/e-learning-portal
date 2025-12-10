ec7bf5f55f231a423d4f04694bc48b4c48c3479b
root <root@LAPTOP-0PBHL1A3.localdomain>
2025-12-10 20:11:22 +0100

    fix views

--- tests/views.py
+++ tests/views.py
@@ -5,7 +5,8 @@ from portal.models import Testy, Pytania, Odpowiedzi
 def test_view(request, pk):
     test = get_object_or_404(Testy, pk=pk)
 
-    pytanie = Pytania.objects.filter(test=test)
+    pytanie = test.Pytania.all().prefetch_related('odpowiedzi')
+
     if not pytanie:
         return render(request, 'tests/test_view.html', {
             'test': test,
@@ -18,4 +19,4 @@ def test_view(request, pk):
         'test': test,
         'pytanie': pytanie,
         'odpowiedzi': odpowiedzi,
-        })
+    })
